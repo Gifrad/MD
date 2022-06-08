08333bff07090e26ae92965603234a9db32791d8
Gilang Fitra Ramadhana <gifrad98@gmail.com>
2022-06-08 22:35:41 +0700

    create function LoginUser in LoginActivity

--- app/src/main/java/com/capstone/project/trashhub/view/login/LoginActivity.kt
+++ app/src/main/java/com/capstone/project/trashhub/view/login/LoginActivity.kt
@@ -4,8 +4,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import com.capstone.project.trashhub.R
 import com.capstone.project.trashhub.databinding.ActivityLoginBinding
+import com.capstone.project.trashhub.view.home.HomeActivity
 import com.capstone.project.trashhub.view.register.RegisterActivity
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.firebase.auth.FirebaseAuth
@@ -46,12 +48,38 @@ class LoginActivity : AppCompatActivity() {
                     return@setOnClickListener
                 }
             }
-
+            loginUser(email, pass)
         }
         binding.btnRegister.setOnClickListener {
             startActivity(Intent(this, RegisterActivity::class.java))
         }
     }
+
+    private fun loginUser(email: String, pass: String) {
+        auth.signInWithEmailAndPassword(email, pass)
+            .addOnCompleteListener(this) {
+                if (it.isSuccessful) {
+                    showLoading(false)
+                    Intent(
+                        this@LoginActivity,
+                        HomeActivity::class.java
+                    ).also { intent ->
+                        intent.flags =
+                            Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                        startActivity(intent)
+                    }
+                    Toast.makeText(this, "Login berhasil", Toast.LENGTH_SHORT).show()
+                } else {
+                    showLoading(false)
+                    Toast.makeText(
+                        this,
+                        "Login gagal Periksa Email dan Password",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+    }
+
 
     private fun showLoading(state: Boolean) {
         if (state) {
