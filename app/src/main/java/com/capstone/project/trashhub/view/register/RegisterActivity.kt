5c666fa8d1462e0a791558648c363ced732b7954
Gilang Fitra Ramadhana <gifrad98@gmail.com>
2022-06-08 22:15:16 +0700

    create function validation editText

--- app/src/main/java/com/capstone/project/trashhub/view/register/RegisterActivity.kt
+++ app/src/main/java/com/capstone/project/trashhub/view/register/RegisterActivity.kt
@@ -1,12 +1,65 @@
 package com.capstone.project.trashhub.view.register
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.capstone.project.trashhub.R
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.capstone.project.trashhub.databinding.ActivityRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        auth = FirebaseAuth.getInstance()
     }
+
+    private fun setupAction() {
+        binding.btnRegister.setOnClickListener {
+            showLoading(true)
+            val name: String = binding.nameEdtText.text.toString().trim()
+            val email: String = binding.emailEdtText.text.toString().trim()
+            val pass: String = binding.passwordEdtText.text.toString().trim()
+            val confPass: String = binding.confPassEdtText.text.toString().trim()
+
+            when {
+
+                name.isEmpty() -> {
+                    showLoading(false)
+                    binding.nameEdtText.error = "Nama tidak boleh kosong"
+                    binding.nameEdtText.requestFocus()
+                    return@setOnClickListener
+                }
+                email.isEmpty() -> {
+                    showLoading(false)
+                    binding.emailEdtText.error = "Email tidak boleh kosong"
+                    binding.emailEdtText.requestFocus()
+                    return@setOnClickListener
+                }
+                pass.isEmpty() -> {
+                    showLoading(false)
+                    binding.passwordEdtText.error = "Password tidak boleh kosong"
+                    binding.passwordEdtText.requestFocus()
+                    return@setOnClickListener
+                }
+                confPass.isEmpty() || confPass != pass -> {
+                    showLoading(false)
+                    binding.confPassEdtText.error = "Silahkan masukan password yang sama"
+                    binding.confPassEdtText.requestFocus()
+                    return@setOnClickListener
+                }
+            }
+        }
+    }
+
+        private fun showLoading(state: Boolean) {
+            if (state) {
+                binding.progressBar.visibility = View.VISIBLE
+            } else {
+                binding.progressBar.visibility = View.GONE
+            }
+        }
 }
